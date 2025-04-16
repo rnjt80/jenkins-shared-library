44e6c6f699546ef5c8ff7e1876972b7c65c7e672
rnjt80 <53264609+rnjt80@users.noreply.github.com>
2025-04-16 12:17:14 +0530

    Update and rename git_clone.groovy to gitClone.groovy

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -1,6 +1,6 @@
 def call(String url, String branch) {
 	withCredentials([usernamePassword(credentialsId: 'github')]) {
 	    echo "Cloning repo from ${url}"
-    	git url: "${url}", branch: "${branch}"
+	    git url: "${url}", branch: "${branch}"
   	}
 }
