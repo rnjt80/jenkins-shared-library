f3953555f6f197f4f0059f2ae2796e24947ddd76
rnjt80 <53264609+rnjt80@users.noreply.github.com>
2025-04-16 17:01:22 +0530

    Update and rename imageVersion.groovy to ImageTag.groovy

--- vars/ImageTag.groovy
+++ vars/ImageTag.groovy
@@ -2,5 +2,5 @@ def call(String versionfile) {
   File = readFile("${versionfile}")
   version = File.toString().tokenize('version:')[1].split('"')[1]
   // Set the environment variable
-  env.IMAGE_VERSION = version
+  env.ImageTag = version
 }
