0773c537d467a045198bc4af89bfa59d378ff866
rnjt80 <53264609+rnjt80@users.noreply.github.com>
2025-04-16 11:47:48 +0530

    Update git_clone.groovy

--- vars/git_clone.groovy
+++ vars/git_clone.groovy
@@ -1,5 +1,5 @@
 def call(String url, String branch) {
-  withCredentials([(credentialsId: 'github')]) {
+  withCredentials([string(credentialsId: 'github')]) {
     echo "Cloning repo from $url"
     git url: "${url}", branch: "${main}"
   }
