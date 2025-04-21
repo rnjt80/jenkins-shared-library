220da8500fbad77c7feea73edbc8433445a895cd
rnjt80 <53264609+rnjt80@users.noreply.github.com>
2025-04-21 11:52:46 +0530

    Update Image.groovy

--- vars/Image.groovy
+++ vars/Image.groovy
@@ -2,5 +2,6 @@ def call(String versionfile, String dockerImageName) {
   def version = readJSON file: "${versionfile}"
   env.ImageTag = version.version
   def dockerImage = docker.build "${dockerImageName}:${env.ImageTag}"
+  echo ${dockerImage}
   return dockerImage
 }
